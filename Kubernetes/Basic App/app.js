54fb26dfcfd0a737d40343df4ab5c43fb3f07a71
Kenji Emura <kenjiemura@gmail.com>
2021-05-12 21:59:18 +0900

    Changed the image

--- Kubernetes/Basic App/app.js
+++ Kubernetes/Basic App/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
-    <h2>This is the image in version 2.0!!!</h2>
+    <h2>This is the image in version 3.0~~~~~~</h2>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
